5da9da0c6369254a8800cd446b0a4d5828f6ea5f
ritulsingh <ritul.mtalkz@gmail.com>
2023-03-01 12:52:18 +0530

    callback for meta apis

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get("/", (req, res) => {
 
 app.post("/waba-meta", (req, res) => {
     let body = req.body;
+    const challenge = request.query['hub.challenge'] || 'OK';
     console.log(JSON.stringify(body, null, 2));
+    reply.status(200).send(challenge)
 })
 
 app.get("/waba-meta", (req, res) => {
