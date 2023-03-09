226aafe4d301d26fc94db1939f1759806a851ab6
ritulsingh <ritul.mtalkz@gmail.com>
2023-03-09 15:16:30 +0530

    Update

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.post("/waba-karix", (req, res) => {
     let body = req.body;
     const challenge = req.query['hub.challenge'] || 'OK';
     console.log(JSON.stringify(body, null, 2));
-    result += (","+JSON.stringify(body, null, 2))
+    result += ("," + JSON.parse(body, null, 2))
     res.status(200).send(challenge)
 })
-app.get("/result", (req, res) =>{
+app.get("/result", (req, res) => {
     res.status(200).send(result)
 })
 
