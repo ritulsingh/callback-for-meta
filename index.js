bef9f54bbe27de2bd8d9c785d3034a1c73e903ca
ritulsingh <ritul.mtalkz@gmail.com>
2023-03-09 10:48:32 +0530

    Update the callback for karix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post("/waba-meta", (req, res) => {
     console.log(JSON.stringify(body, null, 2));
     res.status(200).send(challenge)
 })
-let result;
+let result = {};
 app.post("/waba-karix", (req, res) => {
     let body = req.body;
     const challenge = req.query['hub.challenge'] || 'OK';
